20c121bb0d1208bf3579d2fbdedda73c1fc078bf
Lauren Kessell <lkessell1@gmail.com>
2021-04-14 19:45:33 -0400

    Change all button colors on click

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-var studyButton = document.querySelector('#studyButton');
-var meditateButton = document.querySelector('#meditateButton');
-var exerciseButton = document.querySelector('#exerciseButton');
-var studyIcon = document.querySelector('#studyIcon');
+var studyButton = document.getElementById('studyButton');
+var meditateButton = document.getElementById('meditateButton');
+var exerciseButton = document.getElementById('exerciseButton');
+var studyIcon = document.getElementById('studyIcon');
+var meditateIcon = document.getElementById('meditateIcon');
+var exerciseIcon = document.getElementById('exerciseIcon');
 
 studyButton.addEventListener('click', changeStudyButton);
+meditateButton.addEventListener('click', changeMeditateButton);
+exerciseButton.addEventListener('click', changeExerciseButton);
+
 
 function changeStudyButton() {
   studyButton.classList.add('study-active');
-  studyIcon.src="./assets/study-active.svg";
+  studyIcon.src='./assets/study-active.svg';
+}
+
+function changeMeditateButton() {
+  meditateButton.classList.add('meditate-active');
+  meditateIcon.src='./assets/meditate-active.svg';
+}
+
+function changeExerciseButton() {
+  exerciseButton.classList.add('exercise-active');
+  exerciseIcon.src='./assets/exercise-active.svg';
 }
