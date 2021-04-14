d38eebc2d2525db3a1cf02aa22062e407ac4b401
Lauren Kessell <lkessell1@gmail.com>
2021-04-14 19:46:42 -0400

    Prevent non-numbers in inputs

--- main.js
+++ main.js
@@ -4,10 +4,14 @@ var exerciseButton = document.getElementById('exerciseButton');
 var studyIcon = document.getElementById('studyIcon');
 var meditateIcon = document.getElementById('meditateIcon');
 var exerciseIcon = document.getElementById('exerciseIcon');
+var minuteInput = document.getElementById('minuteField');
+var secondInput = document.getElementById('secondField');
 
 studyButton.addEventListener('click', changeStudyButton);
 meditateButton.addEventListener('click', changeMeditateButton);
 exerciseButton.addEventListener('click', changeExerciseButton);
+minuteInput.addEventListener('keydown', preventInvalidEntry);
+secondInput.addEventListener('keydown', preventInvalidEntry);
 
 
 function changeStudyButton() {
@@ -23,4 +27,12 @@ function changeMeditateButton() {
 function changeExerciseButton() {
   exerciseButton.classList.add('exercise-active');
   exerciseIcon.src='./assets/exercise-active.svg';
+}
+
+function preventInvalidEntry(event) {
+  var invalidChars = ['-', '+', 'e'];
+  if (invalidChars.includes(event.key)) {
+    console.log(event);
+    event.preventDefault();
+  }
 }
